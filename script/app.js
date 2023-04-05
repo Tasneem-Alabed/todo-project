22824e821825c98e33c4c89afe511c27139d760f
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-04-05 20:33:15 +0000

    add three question then put answer in array

--- script/app.js
+++ script/app.js
@@ -1,12 +1,12 @@
 'use strict';
-  let personName=prompt("Enter Your Name !");
-  
-var gender;
-var genderMale="male";
-var genderFemale="female";
-
+ var useData;
+function selectGender(){
   let personGender=prompt("What Is Your Gender ?");
-    if(personGender.toUpperCase()==genderFemale.toUpperCase()) {
+  var gender;
+  var genderMale="male";
+  var genderFemale="female";
+  
+  if(personGender.toUpperCase()==genderFemale.toUpperCase()) {
     gender="Ms";}
   else if(personGender.toUpperCase()==genderMale.toUpperCase()){
     gender="Mr";
@@ -14,8 +14,11 @@ var genderFemale="female";
   else {
      gender=" ";
   }
-
+  return gender;
+}
+function userAge(){
   let personAge=prompt("Enter Your Age!");
+  
   if(personAge==0){
     let mas="Yor Age Is Equal Zero";
     alert(mas);
@@ -23,7 +26,60 @@ var genderFemale="female";
     let mas="Yor Age Is Less Than Zero";
     alert(mas);
   }
+return personAge;
+}
 
-  let welcom=confirm("Do you Want To Skip Welcoming Masseg?");
-  let peersonChoise= (welcom)?"welcom" : alert("Hi "+gender+" "+personName) ; 
+function  gitUserName(){
+   let personName = prompt("Enter Your Name !");
+
+return personName;
+}
  
+function welomeMass(){
+   let name=gitUserName();
+   let gender2= selectGender();
+  let ageUser= userAge();
+  useData=[name , gender2 , ageUser ]
+    let welcom=confirm("Do you Want To Skip Welcoming Masseg?");
+    let peersonChoise= (welcom)?"welcom" : alert("Hi "+ gender2  +" "+ name) ; 
+    return peersonChoise;
+  }
+  welomeMass();
+
+function checkAnswer(dataCome){
+  let answerYey = "Yes";
+  let answerNo = "no";
+  if(answerYey.toUpperCase() == dataCome.toUpperCase()){
+    
+    return "Yes";
+
+  }else if(answerNo.toUpperCase() == dataCome.toUpperCase()){
+    
+    return "No";
+  }else {
+   
+    return "invalid";
+  }
+
+}  
+function getMoreData(){
+     alert ("Please Answer Yes Or No Thies Therr Question");
+    let haveJop= prompt("Do you have A jop ?");
+    let answerJop = checkAnswer(haveJop);
+     useData.push(answerJop);
+    let canDraive = prompt ("Can Yo Drive ?");
+    let answerDrive= checkAnswer(canDraive) ;
+    useData.push(answerDrive);
+    let finishStudy = prompt ("Are You Still Stady Or Not ?")
+    let answerStudy = checkAnswer(finishStudy);
+    useData.push(answerStudy);
+    console.log(answerJop+" " + answerDrive + " "+ answerStudy);
+     
+    console.log(useData);
+  }
+  getMoreData();
+
+   
+
+
+  
